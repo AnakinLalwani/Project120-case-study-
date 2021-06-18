2a34c47f66d49619ec1aba6786357489a39e8871
AnakinLalwani <72091898+AnakinLalwani@users.noreply.github.com>
2021-06-18 19:34:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function gl() {
+    window.location.href = "https://lens.google/";
+}
+function mn() {
+    window.location.href = "https://anakinlalwani.github.io/Class120-121-Reatime-Image-Recognition-M/";
+}
+
+function testpage() {
+    window.location = "test.html"
+}
+
+function back() {
+    window.location = "index.html"
+}
